4d3eb52eae544091d05667d92f39ad923ac94d24
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2022-05-13 14:46:33 -0500

    things, grid being drawn

--- www/index.js
+++ www/index.js
@@ -1,11 +1,58 @@
-import { Universe } from "wasm-game-of-life";
+import { Universe, Cell } from "wasm-game-of-life";
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
+
+const CELL_SIZE = 10; //px
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
 
 const pre = document.getElementById("game-of-life-canvas");
+
+// Construct the universe, and get its width and height.
 const universe = Universe.new()
+const width = universe.width();
+const height = universe.height();
+
+const getIndex = (row, column) => {
+    return row * width + column;
+}
+
+// Give the canvas room for all of our cells and a 1px border
+// around each of them.
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
 
 const renderLoop = () => {
-    pre.textContent = universe.render();
     universe.tick();
+    drawGrid();
+    drawCells();
     requestAnimationFrame(renderLoop);
 }
+
+const drawGrid = () => {
+    ctx.beginPath();
+    ctx.strokeStyle = GRID_COLOR;
+
+    //Vertical lines.
+    for (let i=0; i<=width; i++) {
+        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+    }
+
+    //Horizontal Lines
+    for (let j=0; j<=height; j++) {
+        ctx.moveTo(0,                           j * (CELL_SIZE + 1) + 1);
+        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+    }
+
+    ctx.stroke();
+}
+const drawCells = () => {
+    
+}
+
+
 requestAnimationFrame(renderLoop);
